cb475f3661a0fd7a63f18466cf366f73c3935cf7
joaovitortrilho <joao.trilho@edu.senai.br>
2026-08-05 15:56:55 -0300

    aula02 - 05/08

---
+++ ola_mundo.php
@@ -0,0 +1,9 @@
+<?php
+echo "NONAME";
+print 'joao';
+
+$variavel1 =20;
+$variavel_2 = 300;
+echo"$variavel1 + $variavel_2 =  " . ($variavel1 + $variavel_2);
+
+?>
---
+++ soma.php
@@ -0,0 +1,15 @@
+<?php
+// este é um comentario de uma linha
+
+/*
+ Oque é uma variavel?
+    Uma variável é um espaço na memoria do computador[
+    que armazena um valor
+
+*/
+$variavel1 =20;
+$variavel_2 = 300;
+echo "$variavel1 + $variavel_2 = " . ($variavel1 + $variavel_2)
+
+
+?>
